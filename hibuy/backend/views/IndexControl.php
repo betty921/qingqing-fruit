02a296fe3067abb287326e019a65b07c40eb6a8f
unknown <hui.liu1@sz-cyl-PC.staff.weiphone.com>
2018-05-30 18:33:55 +0800

    remove hibuy

--- hibuy/backend/views/IndexControl.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-时间：2017年8月8日
-
-*/
-header("content-type:text/html;charset=utf-8");
-
-?>
--- hibuy/conf/dababase.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysql = [
-    'host'=>'localhost',
-    'user'=>'root',
-    'password'=>'123',
-    'database'=>'hibuy',
-    'port'=>3306
-];
